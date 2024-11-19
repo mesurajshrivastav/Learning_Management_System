bfe43d2bfad25c5800c78fea86c7c2609df31fad
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-11-19 23:49:12 +0530

    added login functionality in backend

--- backend/routes/auth-routes/index.js
+++ backend/routes/auth-routes/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const { registerUser } = require("../../controllers/auth-controller/index.js");
+const { registerUser,loginUser } = require("../../controllers/auth-controller/index.js");
+const authenticateMiddlware = require('../../middleware/auth-middleware.js')
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post('/check-auth',authenticateMiddlware,(req,res)=>{
+    const user = req.user
+
+    res.status(200).json({
+            success: true,
+            message: 'Authenticate user!',
+            data: {
+                user,
+            }
+    })
+})
 
 module.exports = router;
